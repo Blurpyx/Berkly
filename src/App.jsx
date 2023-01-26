fdabf31f42cf9fb9cd125fa11a4cfee586b84046
Tommy Tytgat <60700831+Blurpyx@users.noreply.github.com>
2023-01-26 14:57:17 +0100

    laptop-desktop sync

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ function App() {
   return (
     <div className='max-w-[80%] mx-auto'>
       <Navbar currentTab="home" />
-      <div className='flex mt-40'>
-        <img className='w-1/2' src="" alt="Berkly svg" />
-        <div>
-          <h1 className='text-3xl font-bold w-1/2 font-title'>AN IMMUTABLE VOICE IN POLLS</h1>
+      <div className='flex mt-40 justify-between'>
+        <div className='max-w-[50%]'>
+          <img className='w-[16.5rem] xl:w-[29.3rem] h-36 xl:h-64 absolute z-1' src="/images/svg-background.jpeg" />
+          <img className='h-36 xl:h-64' src='/images/berkly-to-animate.svg' alt='Berkly' />
+        </div>
+        <div className='max-w-[50%]'>
+          <h1 className='text-3xl font-bold font-title'>AN IMMUTABLE VOICE IN POLLS</h1>
           <p className='text-gray-500'>Berkly is a system of checks and balances to create and distribute verifiable, 
             tamper-proof polls on the blockchain.</p>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,8 +34,8 @@ const Navbar = ({ currentTab }) => {
   };
 
   return (
-    <div className="flex justify-between items-center pt-10">
-      <img className=" h-10" src="/images/berkly-logo.svg" alt="Berkly" />
+    <div className="flex justify-between items-center mt-5">
+      <img className="h-20" src="/images/berkly-logo.svg" alt="Berkly" />
       <ul className="flex">
         <li className="px-5">
           <button className="text-black" id="home" onClick={ChangeSelected}>
